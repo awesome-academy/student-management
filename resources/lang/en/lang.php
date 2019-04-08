60fcc1684606140db849f1f4d4ec72d1b6db52fd
hieptq-0855 <tran.quang.hiep@sun-asterisk.com>
2019-04-08 16:30:06 +0700

    init bower and mix

---
+++ resources/lang/en/lang.php
@@ -0,0 +1,7 @@
+<?php
+    return [
+        'welcome_title' => 'Student Management Page',
+        'welcome_content' => 'Student Management',
+        'welcome_admin' => 'Page for admin',
+        'welcome_student' => 'Page for student',
+    ];
